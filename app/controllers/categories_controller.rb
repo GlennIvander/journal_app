397476d9dc9a4b2d80c8970196a4ba862a4e3c96
GlennIvander <ivander792@gmail.com>
2025-04-12 02:12:29 +0800

    date

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
   before_action :set_id, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: [ :index, :show ]
+  before_action :correct_user, only: [ :edit, :update ]
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActiveRecord::InvalidForeignKey, with: :invalid_foreign_key
 
@@ -14,11 +16,11 @@ class CategoriesController < ApplicationController
   def edit; end
 
   def new
-    @category = Category.new
+    @category = current_user.categories.new
   end
 
   def create
-    @category = Category.new(category_params)
+    @category = current_user.categories.new(category_params)
     if @category.save
       redirect_to @category, notice: "Category: #{@category.title} saved successfully"
     else
@@ -42,6 +44,11 @@ class CategoriesController < ApplicationController
   private
   def category_params
     params.require(:category).permit(:title, :body, :user_id)
+  end
+
+  def correct_user
+    @category = current_user.category.find_by(id: params[:id])
+    redirect_to root_path, notice: "Not authorized to edit this category" if @category.nil?
   end
 
   def set_id
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,9 +12,9 @@ class TasksController < ApplicationController
 
   def update
     if @task.update(task_params)
-      redirect_to category_tasks_path(@category), notice: "Yeey. Update successful."
+      redirect_to category_tasks_path(@category), notice: "Updated successfully."
     else
-      flash[:alert] = "Sad. Update unsuccessful."
+      flash[:alert] = "Oh no! Updated unsuccessfully."
       render :edit, status: :unprocessable_entity
     end
   end
@@ -22,7 +22,7 @@ class TasksController < ApplicationController
   def destroy
     if @task.destroy
       redirect_to category_path(@category), status: :see_other
-      flash[:alert] = "Yeey. Delete successful."
+      flash[:alert] = "Deleted successfully."
     else
       render :edit, status: :unprocessable_entity
     end
@@ -31,16 +31,16 @@ class TasksController < ApplicationController
   def create
     @task = @category.tasks.new(task_params)
     if @task.save
-      redirect_to category_path(@category), notice: "Yeey. Create successful.", status: :see_other
+      redirect_to category_path(@category), notice: "Task created successfully.", status: :see_other
     else
-      flash[:alert] = "Sad. Create unsuccessful."
+      flash[:alert] = "Oh no! Task created unsuccessful."
       redirect_to category_path(@category)
     end
   end
 
   private
   def task_params
-    params.require(:task).permit(:task, :body)
+    params.require(:task).permit(:task, :body, :due_date)
   end
 
   def set_id
